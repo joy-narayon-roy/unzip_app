8c2244b5f89f3fd37b02ad2843ee663656d1526c
Joy Roy <joyroy261216@gmail.com>
2021-06-27 13:01:46 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Python Unzip App 
+------------------
+    This is our 'unzip app' wich is made by Python. You can unzip .zip file without install any App or anything else.
+    You can run it any Terminal. You can run it in android Termux app.
+    You must be Installed Python in your local machine.
+## Install Python 
+```bash
+pkg install python
+```
+## Install Unzip 
+```bash
+```
+## Run the app
+```bash
+python app.py 
+```
---
+++ app.py
@@ -0,0 +1,47 @@
+import os 
+from zipfile import ZipFile 
+
+
+def extractZipFile(fileName):
+    zip = ZipFile(fileName,'r')
+    
+    print("\nAll files :-")
+    zip.printdir()
+    zip.extractall()
+    foldName = os.path.splitext(fileName)[0]
+    print(f"\nSuccessfully Save.\nFolder name :- {foldName}")
+    
+
+def allZipFiles():
+    allFiles = os.listdir()
+    allZipFiles=[]
+    
+    for allFile in allFiles:
+        file = os.path.splitext(allFile)[1]
+        
+        if file == ".zip":
+            allZipFiles.append(allFile)
+    
+    ind = 1
+    print("\nAll Zip file in this folder.")
+    for zipFile in allZipFiles:
+        print(f"{ind} - {zipFile}")
+        ind = ind + 1
+    print("\n")
+    return allZipFiles    
+
+
+zipFiles = allZipFiles()
+
+if len(zipFiles)==0:
+    print(f"No Zip file found !\nPlease save the zip file in this folder and run code.")
+else:
+    selectedFile = int(input("Please input the Zip file index no : "))
+    
+    if selectedFile<=len(zipFiles) :
+        print("\nYou have selected :- ")
+        print(f"{selectedFile} - {zipFiles[selectedFile-1]}")
+        
+        extractZipFile(zipFiles[selectedFile-1])
+    else:
+        print('Please input a valid index.\nRun again.')
